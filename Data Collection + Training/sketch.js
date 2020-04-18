74f2053d2010822fc182037a227fec56b4232550
Ghassen Chaabouni <ghassen1302@live.com>
2020-04-18 13:53:48 +0100

    Update sketch.js

--- Data Collection + Training/sketch.js
+++ Data Collection + Training/sketch.js
@@ -1,14 +1,3 @@
-// ml5.js: Pose Classification
-// The Coding Train / Daniel Shiffman
-// https://thecodingtrain.com/learning/ml5/7.2-pose-classification.html
-// https://youtu.be/FYgYyq-xqAw
-
-// All code: https://editor.p5js.org/codingtrain/sketches/JoZl-QRPK
-
-// Separated into three sketches
-// 1: Data Collection: https://editor.p5js.org/codingtrain/sketches/kTM0Gm-1q
-// 2: Model Training: https://editor.p5js.org/codingtrain/sketches/-Ywq20rM9
-// 3: Model Deployment: https://editor.p5js.org/codingtrain/sketches/c5sDNr8eM
 
 let video;
 let poseNet;
